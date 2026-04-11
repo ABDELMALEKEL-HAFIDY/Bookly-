43f3ae617d2c9181e8e8c06196198add84cfb7c2
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-04-11 19:35:50 +0100

    splash view done

--- lib/feature/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
@@ -6,7 +8,14 @@ class SplashViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text('data')]);
+        SvgPicture.asset(
+          Assets.imagesLogo,
+          width: 70,
+          height: 70,
+          fit: BoxFit.cover,
+          )]);
   }
 }

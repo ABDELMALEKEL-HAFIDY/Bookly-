11ed872431d0e1587ad1507b9e70d913c9a609cc
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-04-11 19:34:18 +0100

    delete theme from scafold

--- lib/feature/spalsh/presentation/views/splash_view.dart
+++ lib/feature/spalsh/presentation/views/splash_view.dart
@@ -8,7 +8,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.scafoldbg,
+     
       body: SplashViewBody(),
       );
   }

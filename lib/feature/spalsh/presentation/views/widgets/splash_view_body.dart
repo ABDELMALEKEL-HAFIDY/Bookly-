8b5bc6a442a59d0fdff4842204e1dec81f4daa62
ABDELMALEKEL-HAFIDY <abdelmalekelhafidy@gmail.com>
2026-04-15 20:43:49 +0100

    singleresponsibility principle and refactor

--- lib/feature/spalsh/presentation/views/widgets/splash_view_body.dart
+++ lib/feature/spalsh/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,11 @@
+import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/feature/home/presentation/views/home_view.dart';
 import 'package:bookly_app/feature/spalsh/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/route_manager.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -9,6 +13,7 @@ class SplashViewBody extends StatefulWidget {
   @override
   State<SplashViewBody> createState() => _SplashViewBodyState();
 }
+
 class _SplashViewBodyState extends State<SplashViewBody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
@@ -18,7 +23,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   void initState() {
     super.initState();
     initSlidingAnimation();
+    navigateToHome();
   }
+
+
+
   @override
   void dispose() {
     super.dispose();
@@ -42,7 +51,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
-
+ //init sliding animation
   void initSlidingAnimation() {
     animationController = AnimationController(
       vsync: this,
@@ -53,5 +62,15 @@ class _SplashViewBodyState extends State<SplashViewBody>
       end: Offset.zero,
     ).animate(animationController);
     animationController.forward();
+  }
+//navigate to home
+    void navigateToHome() {
+     Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kTranstionDuration,
+      );
+    });
   }
 }
